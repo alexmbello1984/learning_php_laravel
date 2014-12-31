ef78c7cdd57c2d730f83c7245672da3f1c33d35b
alexmbello <alexmbello@gmail.com>
2014-12-31 12:05:46 -0500

    archivo helloworld codigo MAL escrito. Problemas: al principio tendriamos al menos unas cinco lineas de codigo de aplicacion( conectarse a la BD, hacer el select y demas logica de negocio) en el mismo archivo con logica de presentacion

--- helloworld.php
+++ helloworld.php
@@ -1,5 +1,12 @@
-<?php
-
-echo "hello world";
-
-?>
+<?php $language = "PHP"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>mejorandophp</title>
+</head>
+<body>
+	<h1>Ejercicio</h1>	
+	<?php echo "Aprendiendo <strong>$language</strong>"; 	?>
+</body>
+</html>
