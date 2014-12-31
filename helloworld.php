9f4b9e6d8fc1a7453935b3ddfe4513779eeca5f4
alexmbello <alexmbello@gmail.com>
2014-12-31 12:21:01 -0500

    Separando logica y presentacion

--- helloworld.php
+++ helloworld.php
@@ -1,12 +1,5 @@
-<?php $language = "PHP"; ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>mejorandophp</title>
-</head>
-<body>
-	<h1>Ejercicio</h1>	
-	<?php echo "Aprendiendo <strong>$language</strong>"; 	?>
-</body>
-</html>
+<?php 
+
+	$language = "PHP"; 
+	
+	require "view.php";
---
+++ view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>mejorandophp</title>
+</head>
+<body>
+	<h1>Ejercicio</h1>	
+	Aprendiendo <strong><?php=  $language ?></strong> 	
+</body>
+</html>
